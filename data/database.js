ddff79c0cae2f01e973b0b1a5bc69b6dc521f1b8
thiago <thiago.w3c@gmail.com>
2024-02-15 00:13:22 -0300

    fix

--- data/database.js
+++ data/database.js
@@ -23,7 +23,7 @@ async function connectToDatabase() {
 // Call the function to connect to the database
 connectToDatabase()
   .then(() => {
-    console.log("Database connection established.");
+    console.log("Database connection established");
   })
   .catch((error) => {
     console.error("Error connecting to database:", error);
